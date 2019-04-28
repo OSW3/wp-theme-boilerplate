1d03171352173157e5ae3e29a0e1867ee607511d
Arnaud BODEL <contact@osw3.net>
2019-04-29 00:36:23 +0200

    -

---
+++ components/menus/menu.php
@@ -0,0 +1,7 @@
+<ul class="<?= $ul_class ?>">
+    <?php foreach (get_menu_by_name( $menu ) as $item): ?>
+    <li class="<?= $li_class ?><?= (get_the_ID() == $item->object_id) ? " active" : ""; ?>">
+        <a class="<?= $a_class ?>" href="<?= $item->url; ?>"><?= $item->title; ?></a>
+    </li>
+    <?php endforeach; ?>
+</ul>
